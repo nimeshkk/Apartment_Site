6d2c4317cd1728715ba1e30d11370b1e4148cdd4
nimeshkk <nimeshkliyanage@gmail.com>
2023-08-12 01:55:01 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './App.css'
 import Navbar from './components/navbar'
+import Home from './components/home/home'
 
 export default function App() {
   return (
     <div>
+
       <Navbar />
+      <Home />
       
       
     </div>
---
+++ src/components/home/home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function home() {
+  return (
+    <div className='home'>
+      <h1>home</h1>
+    </div>
+  )
+}
