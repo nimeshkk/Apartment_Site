738a234b39add1ad56c84bd939a56f9c5a374d00
nimeshkk <nimeshkliyanage@gmail.com>
2023-08-11 15:06:39 +0530

    add

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,27 +1,45 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCrown } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { useRef } from 'react'
 
+export default function navbar() {
 
-export default function nav() {
+  const navRef =useRef();
+
+  const showNavbar =()=>{
+    navRef.current.classList.add('responsive_nav');
+  }
+
   return (
-    <div className='navbar flex'>
+    
+      <div className='navbar'>
 
-       <div className='logo'>
-       <FontAwesomeIcon icon={faCrown} />
-        </div>           
-       <div className='navbarmenu'>
-        <ul className='menu flex'>
-          <li className='listItem'>Home</li>
-          <li className='listItem'>About</li>
-          <li className='listItem'>Contact</li>
-          <li className='listItem'>Blog</li>
-          <li className='listItem'>Portfolio</li>
-        </ul>
-       </div>
-       
+        <div className='logo'>
+          <FontAwesomeIcon icon={faCrown} />
+          </div>
 
+          <nav ref={navRef}>
+            <a href='#'>Home</a>
+            <a href='#'>About</a>
+            <a href='#'>Contact</a>
+            <a href='#'>add</a>
+
+            <button className='nav-btn nav-close-btn' onClick={showNavbar}>
+            <FontAwesomeIcon icon={faXmark} />
+
+            </button>
+
+          </nav>
+
+          <button className='nav-btn' onClick={showNavbar}>
+          <FontAwesomeIcon icon={faBars} />
+          </button>
+      </div>
       
-    </div>
+    
   )
 }
+
