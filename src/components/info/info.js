8a0b706d455c7f7fdc4c53ca3de37f0ff4d3d419
nimeshkk <nimeshkliyanage@gmail.com>
2023-08-18 01:18:43 +0530

    update

--- src/components/info/info.js
+++ src/components/info/info.js
@@ -1,45 +1,61 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import React, { useEffect } from 'react'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
-export default function info() {
+export default function Info() { 
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('view');
+        } else {
+          entry.target.classList.remove('view');
+        }
+      });
+    });
+
+    const hiddenElements = document.querySelectorAll('.show2');
+    hiddenElements.forEach((el) => observer.observe(el));
+
+    return () => {
+      hiddenElements.forEach((el) => observer.unobserve(el));
+    };
+  }, []);
+
   return (
     <div className='info-section'>
-        <div className='info-container'>
-            
-      <div className='cardinfo-grid'>
-        <div className='singleCard-grid'>
+      <div className='info-container show2'>
+        <div className='cardinfo-grid'>
+          <div className='singleCard-grid '>
             <div className='infoIcon'>
-            <FontAwesomeIcon icon={faHouse} />
+              <FontAwesomeIcon icon={faHouse} />
             </div>
-            
             <span className='cardTitle'>dfsfsdfsd</span>
             <p>jhcajhavkhtdht</p>
+          </div>
         </div>
-      </div>
-
-      <div className='cardinfo-grid'>
-        <div className='singleCard-grid'>
-            <div className='infoIcon'>
-            <FontAwesomeIcon icon={faHouse} />
-            </div>
-            <span className='cardTitle'>dfsfsdfsd</span>
-            <p >jhcajhajhgsdjahdjjhvjhzcv</p>
-        </div>
-      </div>
 
         <div className='cardinfo-grid'>
-        <div className='singleCard-grid'>
+          <div className='singleCard-grid'>
             <div className='infoIcon'>
-            <FontAwesomeIcon icon={faHouse} />
+              <FontAwesomeIcon icon={faHouse} />
             </div>
             <span className='cardTitle'>dfsfsdfsd</span>
             <p>jhcajhajhgsdjahdjjhvjhzcv</p>
-        </div>
-        
+          </div>
         </div>
 
+        <div className='cardinfo-grid'>
+          <div className='singleCard-grid '>
+            <div className='infoIcon'>
+              <FontAwesomeIcon icon={faHouse} />
+            </div>
+            <span className='cardTitle'>dfsfsdfsd</span>
+            <p>jhcajhajhgsdjahdjjhvjhzcv</p>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
+
