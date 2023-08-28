eb778a80320bf4bc02be13937b8bb389e8213bbb
nimeshkk <nimeshkliyanage@gmail.com>
2023-08-29 01:30:29 +0530

    update

--- src/components/home/data.js
+++ src/components/home/data.js
@@ -5,18 +5,18 @@ import jpg3 from '../../images/3.jpg'
 const imageSlide=[
     {
         url:jpg1,
-        title:"slide1",
-        body:"jhbjhb",
+        title:"Apartments In SriLanka",
+        body:"Experience a world-class mall right in your neighbourhood. Shop, dine and spend the day with family and friends",
     },
     {
         url:jpg2,
-        title:"slide2",
-        body:"jhbjhb",
+        title:"luxury Bedroom",
+        body:"Balance is always important in interior design.",
     },
     {
         url:jpg3,
-        title:"slide3",
-        body:"jhbjhb",
+        title:"luxury kitchen",
+        body:"One hallmark feature of a luxury kitchen is top-quality appliances, especially if you love to cook and entertain.",
     },
 
 ]
