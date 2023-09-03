20ae1c252758f45c0b438180b784537b488869b0
nimeshkk <nimeshkliyanage@gmail.com>
2023-09-03 19:09:20 +0530

    update

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,7 +1,7 @@
 import { useRef } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaBuilding, FaDragon, FaDrawPolygon, FaHome, FaHorse, FaPhoenixFramework, FaReact, FaTimes } from "react-icons/fa";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChessKnight } from '@fortawesome/free-solid-svg-icons'
+import { faChessKnight, faDragon, faHouse } from '@fortawesome/free-solid-svg-icons'
 
 
 function Navbar() {
@@ -16,17 +16,18 @@ function Navbar() {
 	return (
 		
 		<div className="navbar">
-			<div className="logo"><FontAwesomeIcon icon={faChessKnight} /></div>
+			<div className="logo"><FontAwesomeIcon icon={faDragon} /></div>
 			<nav ref={navRef}>
 				<a href="/#">Home</a>
-				<a href="/#">My work</a>
-				<a href="/#">Blog</a>
-				<a href="/#">About me</a>
+				<a href="/#">Facilities</a>
+				<a href="/#">Info</a>
+				<a href="/#">About</a>
 				<div
 					className="nav-btn nav-close-btn"
 					onClick={showNavbar}>
 					<FaTimes />
 				</div>
+				
 			</nav>
 			<div
 				className="nav-btn"
